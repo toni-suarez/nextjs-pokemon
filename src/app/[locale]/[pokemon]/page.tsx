d68d542fe380dcf2bab299cbbe8051a1d213db36
Toni Suárez <toni.surios@gmail.com>
2024-05-06 09:24:43 +0200

    - added generation hash to back button

--- src/app/[locale]/[pokemon]/page.tsx
+++ src/app/[locale]/[pokemon]/page.tsx
@@ -44,7 +44,7 @@ export default async function Home({
       <div className='max-w-7xl mx-auto pt-20'>
         <Link
           className={`hidden lg:inline-flex text-xs rounded-lg py-2 px-3 ${bgColor} ${textColor} hover:bg-white hover:text-black`}
-          href={`/${locale}`}>
+          href={`/${locale}#${pokemonData.results.generation_id}`}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
             <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
           </svg>
